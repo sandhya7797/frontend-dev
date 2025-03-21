8bb99cd2706e0fe2b4a5b10faf5ec1de14453f1d
Sandhya <ragulapadusandhya7797@gmail.com>
2025-03-21 14:41:26 -0700

    mouseover and mouseleave functionality added and code refactoring done

--- Main/frontend-projects/StarRating/script.js
+++ Main/frontend-projects/StarRating/script.js
@@ -1,20 +1,12 @@
 const starRefs = document.querySelectorAll('.star');
-
-// function toggleStar(refIdx) {
-//     starRefs.forEach((starRef) => {
-//         const starIdx = starRef.querySelector('i').dataset.index;
-//         console.log(starIdx, refIdx);
-//         if(starIdx<= refIdx) {
-//             starRef.classList.add('fa-solid');
-//             starRef.classList.remove('fa-regular');
-//             console.log(starRef.classList);
-//         } else {
-//             starRef.classList.add('fa-regular');
-//             starRef.classList.remove('fa-solid');
-//             console.log(starRef.classList);
-//         }
-//     })
-// }
+let last_selected_starIdx = 0;
+/*
+1. on click we have to select current and previous all stars.
+2. on hover we have to select stars till current star.
+3. on leave we have to deselect all stars until last clicked star.
+eg - clicked on 4th star when we mouseleave to 2nd star it should not get deselected.
+on mouseleave it should always retain last clicked state.
+*/
 
 function selectStar(ref) {
     ref.classList.add('fa-solid');
@@ -28,11 +20,40 @@ function deSelectStar(ref) {
 
 starRefs.forEach((star) => {
     star.addEventListener('click', function(e) {
-        
-        const selectedIdx = e.target.dataset.index;
-        console.log(selectedIdx);
-        for(let i=0;i<selectedIdx;i++) {
-            selectStar(starRefs[i]);
+        const selectedIdx = Number(e.target.dataset.index);
+        last_selected_starIdx = selectedIdx;
+        for(let i=0;i<starRefs.length;i++) {
+            if(i<selectedIdx) {
+                selectStar(starRefs[i].querySelector('i'));
+            } else {
+                deSelectStar(starRefs[i].querySelector('i'));
+            }
         }
     });
+
+    star.addEventListener('mouseover', function(e) {
+        const selectedIdx = Number(e.target.dataset.index);
+        for(let i=0;i<starRefs.length;i++) {
+            if(i<selectedIdx) {
+                selectStar(starRefs[i].querySelector('i'));
+            } else {
+                deSelectStar(starRefs[i].querySelector('i'));
+            }
+        }
+    });
+
+    star.addEventListener('mouseleave', function(e) {
+        starRefs.forEach((ref,idx) => {
+            if(idx>last_selected_starIdx-1) {
+                deSelectStar(ref.querySelector('i'));
+            } else {
+                selectStar(ref.querySelector('i'));
+            }
+        });
+    });
+
+    
 });
+
+
+
