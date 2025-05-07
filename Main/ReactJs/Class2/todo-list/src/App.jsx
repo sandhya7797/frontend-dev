fd6ff9ce12cd7b6dad9da2dd6e2be2462a60b85f
Sandhya <ragulapadusandhya7797@gmail.com>
2025-05-07 14:55:44 -0700

    todo-list app developed using state variables - easy re-rendering

--- Main/ReactJs/Class2/todo-list/src/App.jsx
+++ Main/ReactJs/Class2/todo-list/src/App.jsx
@@ -7,7 +7,7 @@ import TodoList from './components/TodoList'
 function App() {
   return (
     <div>
-      <TodoList title="my-todo" items={["item1","item2","item3","item4","item5"]} myStyles={{color:"red", backgroundColor:"yellow"}}/>
+      {/* <TodoList title="my-todo" items={["item1","item2","item3","item4","item5"]} myStyles={{color:"red", backgroundColor:"yellow"}}/> */}
       <TodoList items={["item6","item7","item8","item9","item10"]}/>
     </div>
   )
