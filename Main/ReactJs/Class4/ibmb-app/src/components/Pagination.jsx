28821cbff3490c0cbcf649b1d200b55f7ab06b1d
Sandhya <ragulapadusandhya7797@gmail.com>
2025-05-16 17:16:39 -0700

    on click highlight page button

--- Main/ReactJs/Class4/ibmb-app/src/components/Pagination.jsx
+++ Main/ReactJs/Class4/ibmb-app/src/components/Pagination.jsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 import "../App.css";
 const Pagination = ({fetchMovies})=> {
     const [list, setList] = useState([1,2,3,4,5]);
+    const [selectedPage, setSelectedPage] = useState(1);
 
     const handleClick = (pageNo) => {
         fetchMovies(pageNo);
+        setSelectedPage(pageNo);
     }
 
     return (
         <div className="pagination-section">
             <button>&lt;</button>
             {
-                list.map((item, index) => (
-                    <button onClick={()=>handleClick(item)}>{item}</button>
+                list.map((page, index) => (
+                    <button className={selectedPage==page?'active':''}onClick={()=>handleClick(page)}>
+                        {page}
+                    </button>
                 ))
             }
             <button>&gt;</button>
