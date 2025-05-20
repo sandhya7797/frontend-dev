01c60b40f51cd63438426548c99f3ed32729d820
Sandhya <ragulapadusandhya7797@gmail.com>
2025-05-20 15:00:32 -0700

    React-Redux basic Counter App

--- Main/ReactJs/Class4/ibmb-app/src/main.jsx
+++ Main/ReactJs/Class4/ibmb-app/src/main.jsx
@@ -1,10 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, RouterProvider } from 'react-router'
 import { StrictMode } from 'react'
+import { store } from './store/index.js'
+import { Provider } from 'react-redux'
+
+//whatever present in store will be available in the entire app
 
 createRoot(document.getElementById('root')).render(
-      <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
 
--- Main/ReactJs/Class4/ibmb-app/src/pages/MovieDetailsPage.jsx
+++ Main/ReactJs/Class4/ibmb-app/src/pages/MovieDetailsPage.jsx
@@ -1,7 +1,10 @@
+import { Counter } from "../store/Counter";
+
 const MovieDetails = () => {
     return (
         <div>
             <h1>Movie Details</h1>
+            <Counter />
         </div>
     );
 }
