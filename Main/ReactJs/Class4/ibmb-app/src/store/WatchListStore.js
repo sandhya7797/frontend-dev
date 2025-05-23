1073d33c994e56788d08b7f5d42ae1743ff21714
Sandhya <ragulapadusandhya7797@gmail.com>
2025-05-22 17:30:22 -0700

    watchlist state added to local storage

--- Main/ReactJs/Class4/ibmb-app/src/store/WatchListStore.js
+++ Main/ReactJs/Class4/ibmb-app/src/store/WatchListStore.js
@@ -1,12 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [];//to maintain list of objects ({id:movie_id,movie:movie})
+// Function to save the state to localStorage
+const saveStateToLocalStorage = (state) => {
+    localStorage.setItem('watchlist', JSON.stringify(state));
+};
+
+// Function to load the state from localStorage (if temporary data exists)
+const loadStateFromLocalStorage = () => {
+    const savedState = localStorage.getItem('watchlist');
+    if (savedState) {
+        return JSON.parse(savedState);
+    }
+}
+
+const initialState = loadStateFromLocalStorage()  || {};//Initialize with an empty object if no saved state is found
 
 export const watchListSlice = createSlice({
     name: 'watchlist',
     initialState,
     reducers: {
         setWatchList: (state, action) => {
+            saveStateToLocalStorage(state);
             return action.payload; // Replace the watchlist with the new array
         },
         // addToWatchList: (state, action) => {
